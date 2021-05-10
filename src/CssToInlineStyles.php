31e3331ec4dc4db8316a13cd8f166abdf57951f9
Jeffrey Zant <j.zant@slash2.nl>
2021-05-10 14:58:35 +0200

    Let the user handle xml errors

--- src/CssToInlineStyles.php
+++ src/CssToInlineStyles.php
@@ -112,9 +112,7 @@ class CssToInlineStyles
     protected function createDomDocumentFromHtml($html)
     {
         $document = new \DOMDocument('1.0', 'UTF-8');
-        $internalErrors = libxml_use_internal_errors(true);
         $document->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
-        libxml_use_internal_errors($internalErrors);
         $document->formatOutput = true;
 
         return $document;
